1d5370c6be818335b09e143b36afe16cf95ebc54
cekys <10346089+cekys@users.noreply.github.com>
2020-09-22 14:08:15 +0800

    Update var.go

--- var.go
+++ var.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	encoders       = []string{"ffmpeg", "cmd"}
+	encoders       = []string{"ffmpeg", "null"}
 	extensions     = []string{".flv", ".ts", ".m3u8"}
 	workingPath, _ = os.Getwd()
 	sub            = false
