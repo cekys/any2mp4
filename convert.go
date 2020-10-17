24f3207a4e2b599fc0737706e17d20ab3834a38e
cekys <10346089+cekys@users.noreply.github.com>
2020-10-17 19:19:51 +0800

    update
    多线程支持

---
+++ convert.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"github.com/cekys/gopkg"
+	"os"
+	"os/exec"
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+//Find files with a specific suffix
+func findFilesWithSuffix(fileList *list.List, suffix []string) *list.List {
+	returnList := list.New()
+	file := fileList.Front()
+
+	for i := 0; i < fileList.Len(); i++ {
+		for _, ext := range suffix {
+			if strings.HasSuffix(file.Value.(string), ext) {
+				returnList.PushBack(file.Value)
+			}
+		}
+		file = file.Next()
+	}
+	return returnList
+}
+
+func convert2mp4(fileList *list.List, mode int) error {
+	listNode := fileList.Front()
+	// 按cpu数目设置最大协程数目
+	max := runtime.NumCPU()
+	// 协程数组
+	chs := make([]chan string, max)
+	// 声明协程并启动
+	for i := 0; i < max; i++ {
+		chs[i] = make(chan string)
+		go func(ch chan string) {
+			ch <- "start"
+		}(chs[i])
+	}
+
+	// 声明selectCase[]
+	var selectCase = make([]reflect.SelectCase, max)
+	// 给每个selectCase 设置目的地和对应的信道
+	for i := 0; i < max; i++ {
+		selectCase[i].Dir = reflect.SelectRecv
+		selectCase[i].Chan = reflect.ValueOf(chs[i])
+	}
+
+	for i := 0; i < fileList.Len(); i++ {
+		inputFile := listNode.Value.(string)
+		outputFile := gopkg.StringTrimSuffix(inputFile) + ".mp4"
+		chosen, _, recvOk := reflect.Select(selectCase)
+
+		if recvOk {
+			go runit(inputFile, outputFile, mode, chs[chosen])
+		}
+
+		listNode = listNode.Next()
+	}
+	for _, ch := range chs {
+		<-ch
+	}
+	return nil
+}
+
+func runit(inputFile string, outputFile string, mode int, ch chan string) error {
+	cmd := exec.Command(encoders[0], "-i", inputFile, "-codec", "copy", outputFile)
+	err := gopkg.ProgramRealtimeOutput(cmd)
+	if err != nil {
+		return err
+	}
+
+	//If mode = 1 and the cmd completed without errors, delete the input file
+	if mode == 1 {
+		fmt.Println("File:", inputFile, "converted successfully")
+		fmt.Println("Remove the file:", inputFile)
+		err = os.Remove(inputFile)
+		if err != nil {
+			return err
+		}
+	}
+	ch <- "done"
+	return nil
+}
--- main.go
+++ main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"container/list"
+	"flag"
 	"github.com/cekys/gopkg"
 	"log"
+	"os"
 	"path/filepath"
 )
 
+var (
+	encoders       = []string{"ffmpeg"}
+	extensions     = []string{".flv", ".ts", ".m3u8"}
+	workingPath, _ = os.Getwd()
+	sub            = false
+)
+
 func main() {
+	// 设置flag
+	flag.StringVar(&workingPath, "p", "./", "working path")
+	flag.BoolVar(&sub, "sub", false, "sub folder")
+	flag.Parse()
 
-	setupFlags()
-
-	err := checkEncoder()
-	if err != nil {
-		log.Fatal(err)
+	// 检查编码器是否存在
+	for i, encoder := range encoders {
+		encoder, err := gopkg.ProgramExist(encoder)
+		if err != nil {
+			log.Fatal(err)
+		}
+		encoders[i] = encoder
 	}
 
 	fileList := list.New()
-	inputList := list.New()
-	var filter []string
+	filter := make([]string, 0)
 
 	//Find all files in working path
-	err = gopkg.PathWalk(workingPath, fileList, filter, sub)
+	err := gopkg.PathWalk(workingPath, fileList, filter, sub)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//Find all files in the directory with the specified suffix
-	inputList = findFilesWithSuffix(fileList, extensions)
+	inputList := findFilesWithSuffix(fileList, extensions)
 
 	//Check if input files exists
 	if inputList.Len() == 0 {
